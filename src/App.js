97b08d6ac4f3fb25504d47c3ccdf28f2d12cb654
なるpro <t.naruhiro.1026@gmail.com>
2022-06-29 14:58:24 +0900

    ref: 計算式を追加”

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { fetchImages } from "./api";
+import { calculateNumber } from "./calculate";
+import background from "./image/background.jpeg";
+import { useForm } from 'react-hook-form';
 
 function Header() {
 	return (
-		<header className="hero is-dark is-hold">
+		<header className="hero is-small is-info is-hold">
 			<div className="hero-body">
 				<div className="container">
-					<h1 className="title">わんちゃん！画像！</h1>
+					<h1 className="title">家族と遊ぼう</h1>
 				</div>
 			</div>
 		</header>
@@ -48,55 +51,109 @@ function Gallery(props) {
 }
 
 function Form(props) {
-	function handleSubmit(event) {
-		event.preventDefault();
-		const { breed } = event.target.elements;
-		props.onFormSubmit(breed.value);
-	}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    props.onFormSubmit(data);
+  }
+
 	return (
-		<div>
-			<form onSubmit={handleSubmit}>
-				<div className="field has-addons">
-					<div className="control is-expanded">
-						<div className="select is-fullwidth">
-							<select name="breed" defaultValue="shiba">
-								<option value="shiba">しば！</option>
-								<option value="akita">秋田ちゃん</option>
-								<option value="african">アフリカの漢</option>
-								<option value="chow">ちゃうちゃう</option>
-								<option value="samoyed">さもえどちゃん</option>
-							</select>
-						</div>
-						<div className="control">
-							<button type="submit" className="button is-dark">
-								Reload
-							</button>
-						</div>
-					</div>
-				</div>
+		<div className="columns has-background-white is-mobile is-centered">
+			<form onSubmit={handleSubmit(onSubmit)}>
+        <div className="column">
+          <div className="native-flex">
+            <div className="field">
+              <label className="label">ペットの種類</label>
+              <div className="control is-expanded">
+                <div className="select is-rounded is-focused is-info">
+                  <select {...register("animalKind", { required: true })} defaultValue="shiba">
+                    <option value="shiba">いぬ</option>
+                    <option value="akita">ねこ</option>
+                    <option value="african">アフリカの漢</option>
+                    <option value="chow">ちゃうちゃう</option>
+                    <option value="samoyed">さもえどちゃん</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">ペットの年齢（年）</label>
+              <div className="control is-expanded">
+                <input
+                  className="input is-rounded is-focused is-info"
+                  {...register('petAge', { min: 0, max: 30 })}
+                />
+                {errors.petAge && "0から30才の間で選んでください"}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="column">
+          <div className="native-flex">
+            <div className="field">
+              <label className="label">年に何回いっしょに遊びますか？</label>
+              <div className="control is-expanded">
+                <input
+                  className="input is-rounded is-focused is-info"
+                  {...register('numberOfPlay', { required: true })}
+                />
+                {errors.numberOfPlay && "遊ぶ回数を選んでください"}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="icon is-small is-left">
+          <i className="fas fa-globe"></i>
+        </div>
+        <div className="control">
+          <button type="submit" className="button is-primary">
+            結果
+          </button>
+        </div>
 			</form>
 		</div>
 	)
 }
 
+function Result(props) {
+  const { resultNum } = props;
+  return (
+    <div className="columns is-vcentered">
+      {resultNum}
+    </div>
+  );
+}
+
 function Main() {
 	const [urls, setUrls] = useState(null);
+  const [resultNum, setResultNum] = useState(null);
 	useEffect(() => {
     fetchImages("shiba").then((urls) => {
       setUrls(urls);
     });
   }, []);
 
-	function reloadImages(breed) {
-		fetchImages(breed).then((urls) => {
+	function reloadResults(data) {
+		fetchImages(data.animalKind).then((urls) => {
 			setUrls(urls);
 		});
-	}
+
+    calculateNumber(data).then((resultNum) => {
+      setResultNum(resultNum);
+    });
+	};
 	return (
 		<main>
 			<section className="section">
 				<div className="container">
-					<Form onFormSubmit={reloadImages}/>
+					<Form onFormSubmit={reloadResults}/>
+				</div>
+        <div className="container">
+					<Result resultNum={resultNum}/>
 				</div>
 			</section>
 			<section className="section">
@@ -112,9 +169,18 @@ function Footer() {
 	return (
 		<footer className="footer">
 			<div className="content has-text-centered">
-				<p>Dog image are retrieced from Dog API</p>
+				<b>なぜやるか？</b>
 				<p>
-					<a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
+					僕自身ペットわんちゃんを飼ってきました。小さい頃から一緒に成長してきて家族同然に考えていました。
+				</p>
+        <p>
+					しかし、中学生の頃体調が急変して亡くなってしまいます。
+				</p>
+        <p>
+					今考えると犬の寿命は人よりも短く、寿命が近いのはわかりきったことでした。しかし、手遅れになって残ったのは大きな後悔の念だけです。
+				</p>
+        <p>
+          もっと一緒に遊んであげたかった。そんな悔しい想いを抱く人を増やさないタメにも。一緒に遊んで欲しかったペットのためにもこのサイトを作っています。
 				</p>
 			</div>
 		</footer>
@@ -122,8 +188,16 @@ function Footer() {
 }
 
 function App() {
+  const containerStyle = {
+    backgroundImage: `url(${background})`,
+    backgroundPosition: "center center",
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+    backgroundSize: 'cover',
+    backgroundColor: '#464646'
+  }
 	return (
-		<div>
+		<div style={containerStyle}>
 			<Header />
       <Main />
       <Footer />
---
+++ src/calculate.js
@@ -0,0 +1,7 @@
+export async function calculateNumber(data) {
+  var resultNum = 0;
+  if (data.animalKind === 'shiba') {
+    resultNum = (12 - data.petAge) * data.numberOfPlay;
+  };
+	return resultNum;
+}
