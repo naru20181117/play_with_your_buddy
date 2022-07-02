cbdfbb5896aa1af2530dda5c5fc235b8f5e3b7c1
なるpro <t.naruhiro.1026@gmail.com>
2022-07-03 01:31:05 +0900

    change: fontawesomeの導入

--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,8 @@ import { calculateNumber } from "./calculate";
 import { Form } from "./form";
 import { Gallery } from "./gallery";
 import { Result } from "./result";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid, brands } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 export function Main() {
   const [urls, setUrls] = useState(null);
@@ -37,10 +39,30 @@ export function Main() {
       <Result resultNum={resultNum} />
       <div className="column has-text-centered">
         <button onClick={defaultResult} className="button is-primary">
-          スタートに戻る
+          <FontAwesomeIcon icon={solid('rotate-right')} size="xl" className="mr-2" />スタートに戻る
         </button>
       </div>
       <hr/>
+      <div className="column has-text-centered">
+        <p className="mb-4">シェアして応援する</p>
+        <div className="columns is-gapless is-centered">
+          <div className="column is-one-quarter">
+            <a href="https://twitter.com/intent/tweet?text=ペットと何回遊べるかを再認識するサイト&hashtags=playwithyourbuddy&via=1026NT&related=1026NT&url=https://www.playwithyourbuddy.com/" target="_blank" className="has-text-black">
+              <FontAwesomeIcon icon={brands('twitter-square')} size="3x" />
+            </a>
+          </div>
+          <div className="column is-one-quarter">
+            <a href="https://www.facebook.com/share.php?u=https://www.playwithyourbuddy.com/" target="_blank" className="has-text-black">
+              <FontAwesomeIcon icon={brands('facebook-square')} size="3x" />
+            </a>
+          </div>
+          <div className="column is-one-quarter">
+            <a href="https://timeline.line.me/social-plugin/share?url=https://www.playwithyourbuddy.com/" target="_blank" className="has-text-black">
+              <FontAwesomeIcon icon={brands('line')} size="3x" />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 
