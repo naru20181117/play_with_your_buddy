565062129b7093bf09a6b6181e6cf8850838d911
なるpro <t.naruhiro.1026@gmail.com>
2022-07-03 17:49:58 +0900

    set: 画像を挿入/faviconやogpの設定

--- src/header.js
+++ src/header.js
@@ -1,12 +1,14 @@
 import React from "react";
+import title_img from './image/playwithyourbuddy_title.png';
+import headerStyles from './styles/header.module.css';
 
 export function Header() {
   return (
-    <header className="hero is-small is-info is-hold">
+    <header className="hero is-small is-hold">
       <div className="hero-body">
         <div className="container has-text-centered">
-          <h1 className="title">家族と遊ぼう</h1>
-          <h2 className="subtitle">ペットと何回遊べるかを確認しましょう</h2>
+          <img src={title_img} alt="title-img" className={headerStyles.title_img} />
+          <h2 className="subtitle">ペットとあと何回遊べるかを確認しよう</h2>
         </div>
       </div>
     </header>
